59e2d2b215cdd56629575add10f34d245c8b1b83
Rahul Kumar <mars.quantum13@gmail.com>
2024-07-15 15:40:03 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
+
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "668c22f8c473a87fb06b8537",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data was initialized ");
+};
+
+initDB();
